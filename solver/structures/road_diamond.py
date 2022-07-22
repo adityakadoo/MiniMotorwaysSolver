dc16fc2e2fb81496b3987714b03f817892fb35e0
suvenrj <99804277+suvenrj@users.noreply.github.com>
2022-07-22 09:28:22 +0530

    Wrong comments removed.

--- solver/structures/road_diamond.py
+++ solver/structures/road_diamond.py
@@ -10,8 +10,6 @@ class Road_Diamond(Diamond_cell):
         super().__init__(g, node_labels)
 
     def __call__(self, active_dir) -> None:
-        # Only one direction should be active
-        # Nodes for rest of the directions can be deleted
         n_active = 0
         for direction, active in active_dir.items():
             if not active:
